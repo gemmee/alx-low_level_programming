ca6ba986d300886dd0e3cfdfd367c6abd737d840
GEMMEE <jr.gamachu@gmail.com>
2022-03-18 15:53:56 +0000

    0x01_v task4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for(n = 97; n < 123; n++)
+	{
+		if (n == 101 || n == 113)
+			continue;
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
