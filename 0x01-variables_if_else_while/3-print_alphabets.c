b6bc011e95f63042f6b551844c879ef3103192ea
GEMMEE <jr.gamachu@gmail.com>
2022-03-18 14:49:43 +0000

    0x00_v task3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 123; a++)
+		putchar(a);
+	for (a = 65; a < 91; a++)
+		putchar(a);
+	putchar(10);
+
+	return (0);
+}
+
