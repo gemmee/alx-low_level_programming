eed2a5094bf10916017f5769e8d771461636d534
GEMMEE <jr.gamachu@gmail.com>
2022-03-18 09:47:53 +0000

    0x00_c task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return 0: Always Success
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
