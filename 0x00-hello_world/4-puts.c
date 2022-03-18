952cb5250337b02fae57299807be0847ef24fa7b
GEMMEE <jr.gamachu@gmail.com>
2022-03-18 09:14:44 +0000

    0x00_c task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
  * main - Entry point
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
