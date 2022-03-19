23b51a88c93c10e05d67af33f26323a484662a3e
GEMMEE <jr.gamachu@gmail.com>
2022-03-19 12:11:23 +0000

    0x02.C task2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+}
+
