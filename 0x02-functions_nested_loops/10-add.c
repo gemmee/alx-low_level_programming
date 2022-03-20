727942a4072b4681aa75f71b95feaae9615350e2
GEMMEE <jr.gamachu@gmail.com>
2022-03-20 14:19:12 +0530

    0x02.C task10 done

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * add - a function that adds two inegers
- *
+ * add - a function that adds two integers
+ * @a - first number
+ * @b - second number
  * Return: the sum of the two integers
  */
 int add(int a, int b)
 {
 	return (a + b);
 }
-
-
