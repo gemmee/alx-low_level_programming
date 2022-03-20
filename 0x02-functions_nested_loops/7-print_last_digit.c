f07e2d2c7e8abbe163a5fb27db8a2e79fa19048d
GEMMEE <jr.gamachu@gmail.com>
2022-03-20 10:40:47 +0530

    0x02.C task7 done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,9 @@ int print_last_digit(int n)
 {
 	int ld;
 
-	if (n >= 0)
-		ld = n % 10;
-	else
-		ld = -n % 10;
+	ld = (n % 10);
+	if (ld < 0)
+		ld = -1 * ld;
 	_putchar(ld + '0');
 	return (ld);
 }
