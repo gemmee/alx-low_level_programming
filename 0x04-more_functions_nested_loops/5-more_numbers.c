534eff5686cfa9c81159e5c00b5dc7322e5461e2
GEMMEE <jr.gamachu@gmail.com>
2022-04-01 10:43:39 +0530

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,19 @@
  */
 void more_numbers(void)
 {
-int i, j, r, q;
+	int i, j, r, q;
 
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 15; j++)
-{
-r = j % 10;
-q = j / 10;
-if (j > 9)
-_putchar(q + '0');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			r = j % 10;
+			q = j / 10;
 
-_putchar(r + '0');
-}
-_putchar('\n');
-}
+			if (j > 9)
+				_putchar(q + '0');
+			_putchar(r + '0');
+		}
+		_putchar('\n');
+	}
 }
