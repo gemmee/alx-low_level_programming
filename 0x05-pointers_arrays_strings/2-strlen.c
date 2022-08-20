04e7b2e413e945651fc7c85e494bcfbe24be0501
GEMMEE <jr.gamachu@gmail.com>
2022-08-20 15:23:40 +0530

    strlen added

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,17 +4,17 @@
  * _strlen - returns the length of a string
  * @s: pointer to the string
  *
- * return: Always 0.
+ * Return: length of the string.
  */
 
 int _strlen(char *s)
 {
 int i = 0;
 
-while(s[i] != '\0')
+while (s[i] != '\0')
 {
 i++;
 }
 
-return i;
+return (i);
 }
