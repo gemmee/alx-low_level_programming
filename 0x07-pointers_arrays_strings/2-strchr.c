2b1e9fef75b824c0f3c706e8a1f4616d7b66d3ff
GEMMEE <jr.gamachu@gmail.com>
2022-08-23 16:02:59 +0530

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,9 +4,9 @@
   * _strchr - locates a character in a string
   * @s: pointer to the string
   * @c: character to be searched for
-  * 
+  *
   * Description: the function behaves like the standard library fuction strchr.
-  * Return: pointer to the first occurrence of the character c in the string, 
+  * Return: pointer to the first occurrence of the character c in the string,
   * or NULL if the character is not found.
   * author: Jaba
   * date: Aug. 23, 2022. Central Library
