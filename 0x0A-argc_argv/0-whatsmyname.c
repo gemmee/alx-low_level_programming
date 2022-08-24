14eae1d2a5600b499569420a6a503c83061be211
GEMMEE <jr.gamachu@gmail.com>
2022-08-24 20:20:32 +0530

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of this program
+ * @argc: counts number of arguments passed in the command line
+ * @argv: stores arguments passed in the command line
+ *
+ * Return: 0 on success
+ * Author: Jaba
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
