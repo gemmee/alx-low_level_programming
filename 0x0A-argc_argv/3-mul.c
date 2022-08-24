cceb4aee480761a5316eff92ef7f97c8b3b1fb80
GEMMEE <jr.gamachu@gmail.com>
2022-08-25 03:32:17 +0530

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the product of two numbers, followed by a new line.
+ * @argc: number of arguments received.
+ * @argv: array of size argc, containing all arguments received.
+ *
+ * Return: 0 on success, 1 on error.
+ * Author: Jaba
+ * Date: Aug 24 2022. @CL
+ */
+int main(int argc, char *argv[])
+{
+	int count, product;
+
+	product = 1;
+	if (argc == 3)
+	{
+		for (count = 1; count < argc; count++)
+			product *= atoi(argv[count]);
+		printf("%d\n", product);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
+
