1fb94475c4b3028c16429297b1da1eb96d18a673
GEMMEE <jr.gamachu@gmail.com>
2022-08-29 22:53:25 +0530

    dog struct created

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct - a dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
