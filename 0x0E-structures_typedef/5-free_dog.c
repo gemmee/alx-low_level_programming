e66efd1dc4c6e5cdb858e0c774ebc344130906c1
GEMMEE <jr.gamachu@gmail.com>
2022-08-30 10:06:59 +0530

    freed my dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free-dog - frees dogs struct.
+ * @d: pointer to dog struct.
+ *
+ * Return: void.
+ * Author: Jaba
+ * Date: Aug 29 2022 @KP7B -21 room(2nd F)
+ */
+void free_dog(dog_t *d)
+{
+	if (d == 0)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
