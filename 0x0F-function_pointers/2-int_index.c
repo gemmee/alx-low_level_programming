dd6b9cd39825d0ecf3d4db85c883f20ed6e7eae8
GEMMEE <jr.gamachu@gmail.com>
2022-08-30 19:34:51 +0530

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,39 @@
+/**
+  *int_index - searches for an integer.
+  *@array: array of integers.
+  *@size: number of elements in array.
+  *@cmp: pointer to function used to compare values.
+  *
+  *Return: index of first element.
+  *-1 if not element is found or size <= 0.
+  *Author: Jaba
+  *Date: Aug 29 2022 @kp10b
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res;
+
+	res = -1;
+	if (array && cmp)
+	{
+		if (size <= 0)
+		{
+			return (res);
+		}
+		for (i = 0; i < size; i++)
+		{
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
+			{
+				res = i;
+				break;
+			}
+			if (cmp(array[i]) == (-1))
+			{
+				return (res);
+			}
+		}
+	}
+	return (res);
+}
+
