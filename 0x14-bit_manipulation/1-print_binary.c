a1091c5761bbfc7ba4a78a3593d092d1ddbdc652
GEMMEE <jr.gamachu@gmail.com>
2022-08-31 23:40:56 +0530

    prints binary of decimal

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_binary - print binary representation of a number.
+  * @n: decimal number to be printed as binary
+  *
+  * Return: void
+  * Author: Jaba
+  * Date: Aug 31, 2022 @KP10B
+  */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+	int shifts;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
+
