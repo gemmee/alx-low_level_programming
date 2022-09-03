bad273a4bb719f7ff93d63f364b74795700e2fe4
GEMMEE <jr.gamachu@gmail.com>
2022-09-03 17:59:57 +0530

    prints its own name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *main - prints name of file.
+  *
+  *Return: 0.
+  *Author: Jaba
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
