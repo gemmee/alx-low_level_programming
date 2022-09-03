68ee32749729b340298f54b8233fdf128f140550
GEMMEE <jr.gamachu@gmail.com>
2022-09-03 12:38:43 +0530

    get size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - entry point
+ * @void: no arguments
+ * Description: the program starts and ends here.
+ * Return: value 0 to exit
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)", sizeof(char));
-	printf("\nSize of an int: %d byte(s)", sizeof(int));
-	printf("\nSize of a long int: %d byte(s)", sizeof(long));
-	printf("\nSize of a long long int: %d byte(s)", sizeof(long long));
-	printf("\nSize of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
-
