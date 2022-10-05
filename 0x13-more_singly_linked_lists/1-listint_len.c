23fa0bd91b90c5a98ee60387497aa84a455593cd
GEMMEE <jr.gamachu@gmail.com>
2022-10-05 18:41:54 +0530

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked list
+ * @h: pointer to the head of the list listint_t
+ *
+ * Return: the no. of elements
+ * Author: Jaba
+ * Date: Oct 05, 2022 @kiit my 21 room
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		++i;
+		h = h->next;
+	}
+	return (i);
+}
