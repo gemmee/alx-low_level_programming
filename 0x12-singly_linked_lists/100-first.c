3448fe4312b87c2795eb8c8457fe9eb10688ac67
GEMMEE <jr.gamachu@gmail.com>
2022-10-05 12:36:56 +0530

    let's print before main()

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,14 +2,15 @@
 
 /**
  * before_main - a function that executes before main
- * 
+ *
  * Return: none
  * Author: Jaba
  * Date: Oct 05, 2022 @kp10b- 2b21
  */
 
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
 
