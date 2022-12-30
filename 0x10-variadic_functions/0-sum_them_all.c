a04b4d45060d62e9989faaddb50e056ed7ff8217
Jaba <jr.gamachu@gmail.com>
2022-12-30 19:34:18 +0530

    Sum all of variadic arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,8 +6,8 @@
   * all its parameters
   * @n: number of arguments to be summed
   *
-  * Return: sum of all its arguments
-  * Author: Jaba
+  * Return: sum of all its arguments or 0 if n == 0
+  * Author: Jeba
   * Date: Aug 29 2022 @KP 10B
   */
 int sum_them_all(const unsigned int n, ...)
@@ -22,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		int value = va_arg(ap, unsigned int);
+		unsigned int value = va_arg(ap, unsigned int);
 
 		sum += value;
 	}
@@ -30,4 +30,3 @@ int sum_them_all(const unsigned int n, ...)
 	va_end(ap);
 	return (sum);
 }
-
