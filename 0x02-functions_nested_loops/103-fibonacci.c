0a8e97cf6ebeddf124bbdc7210150a6fb0671cf7
Jaba <jr.gamachu@gmail.com>
2023-01-07 21:36:10 +0530

    Adds all even valued fibonacci less than 4m

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints the sum of all even-valued fibonacci terms that starts with 1 and 2,
+ * whose values do not exceed 4 million.
+ * followed by a new line
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+	long int a, b, sum;
+
+	sum = 0;
+	a = 1;
+	b = 2;
+	while (b < 4000000)
+	{
+		if (b % 2 == 0)
+			sum += b;
+		b = a + b;
+		a = b - a;
+	}
+	printf("%ld\n", sum);
+}
