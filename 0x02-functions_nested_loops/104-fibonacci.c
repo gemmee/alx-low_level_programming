073a46419bae66f9c039969136389c83eb309033
Jaba <jr.gamachu@gmail.com>
2023-01-08 13:14:12 +0530

    Prints the first 100 fibonacci, the hard way

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,46 @@
 #include "main.h"
 #include <stdio.h>
 
+#define CUT 10000000
 /**
- * main - Entry point
- * Prints the first 98 fibonacci numbers, starting with 1 and 2,
- * followed by a new line.
+ * main - Entry point, prints the first 98 fibonacci numbers,
+ * starting with 1 and 2, followed by a new line.
+ * I cut my numbers arbitrarily at 10^7 and deal with them as
+ * two separate parts - front and back
  *
  * Return: 0 on success
  */
 int main(void)
 {
-	unsigned long int a, b, count;
+	long int b1, b2, f1, f2;
+	int quo, count;
 
-	a = 1;
-	b = 2;
-	count = 0;
-	printf("%lu, ", a);
+	b1 = 1;
+	b2 = 2;
+	f1 = f2 = 0;
+	count = quo = 0;
+	printf("%ld, %ld, ", b1, b2);
 	while (count < 96)
 	{
-		printf("%lu, ", b);
-		b = b + a;
-		a = b - a;
+		f2 = f2 + f1;
+		f1 = f2 - f1;
+		b2 = b2 + b1;
+		b1 = b2 - b1;
+		if (b2 / CUT > 0)
+		{
+			quo = b2 / CUT;
+			b2 = b2 % CUT;
+		}
+		f2 = f2 + quo;
+		if (f2 > 0)
+			printf("%ld%07ld", f2, b2);
+		else
+			printf("%ld", b2);
+		quo = 0;
+		if (count < 95)
+			printf(", ");
 		count++;
 	}
-	printf("%lu\n", b);
+	printf("\n");
 	return (0);
 }
