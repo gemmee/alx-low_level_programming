0bb0cbe96f18ee7c9b21cc16ed4778ff9fd85ddd
jeba <jr.gamachu@gmail.com>
2023-08-31 04:34:48 +0530

    array_range fixed betty bugs

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,20 +8,20 @@
  * Return: pointer to the newly created array or NULL if min > max or if
  * malloc fails
  */
-int *array_range(int min, int max) {
+int *array_range(int min, int max)
+{
 	int i, num;
 	int *p;
-	
+
 	if (min > max)
 		return (NULL);
 	num = max - min + 1;
-	p = malloc(sizeof *p * num);
+	p = malloc(sizeof(*p) * num);
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < num; i++) {
+	for (i = 0; i < num; i++)
 		*(p + i) = min++;
-	}
+
 	return (p);
 }
-		
