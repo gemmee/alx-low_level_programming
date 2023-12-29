939ff2aae1d18ee87d5fd7ce105666f7c111063b
Gamachu <jr.gamachu@gmail.com>
2023-12-29 10:52:42 +0530

    Prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,16 @@
  * main - Entry point
  * Desc: print "_putchar" followed by a newline
  * Return: 0 on success
+ * Author: Gamachu
  */
 int main(void)
 {
 	int n = 0;
-	char alx[10] = "_putchar\n";
+	char p[10] = "_putchar\n";
 
 	while (n < 9)
 	{
-		_putchar(alx[n]);
+		_putchar(p[n]);
 		n++;
 	}
 	return (0);
