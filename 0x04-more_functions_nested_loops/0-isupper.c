8008819896f01b4649eed68c43301753de6c2a65
Gamachu <jr.gamachu@gmail.com>
2024-01-05 21:20:04 +0530

    Check for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,16 @@
 #include "main.h"
-/**
- * _isupper - evaluate if letter is uppercase
- *@c: print int
- * Return: Always 0
- */
 
+/**
+ * _isupper - checks for uppercase character.
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is uppercase
+ *         0 otherwise
+ * Author: Gamachu AD
+ */
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
