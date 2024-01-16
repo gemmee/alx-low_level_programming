8a2af185f8ec451c2ac64b56d0d22e0c2732ccda
Gamachu <jr.gamachu@gmail.com>
2024-01-16 20:19:24 +0530

    Compute the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,10 +3,10 @@
 /**
  * _strlen_recursion - computes the length of the string
  * @s: pointer to the string
+ * Hint: man strlen
  *
  * Return: the length of the string
- * author: Jaba
- * date: Aug 23, 2022. kp7c
+ * Author: Gamachu AD
  */
 int _strlen_recursion(char *s)
 {
