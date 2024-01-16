858b79028e598a6a1c4d8212b156c9c7e032b7f0
Gamachu <jr.gamachu@gmail.com>
2024-01-16 23:38:06 +0530

    Compare strings having wild character

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,25 +5,21 @@
  * if the strings can be considered identical
  * @s1: the normal string.
  * @s2: the special string containing "*"
+ * Description: the special char * can replace any string, including ''(empty)
  *
  * Return: 1 if identical, else 0
- * Author: Jaba @ Kiit on Oct 03, 2022. My room 21 KP10B
- * I found this code super fascinating - just imagining how it works!
+ * Author: Gamachu AD
  */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s2 == '\0' && *s1 == '\0')
 		return (1);
-
 	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
 		return (0);
-
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) ||  wildcmp(s1 + 1, s2));
-
 	return (0);
 }
 
