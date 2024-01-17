e08f0e514374973be6ecdec21c3a327407234cc2
Gamachu <jr.gamachu@gmail.com>
2024-01-17 17:23:26 +0530

    The man who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated
- * space in memory which contains a copy of the string
- * passed
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ *           which contains a copy of the string passed
  * @str: pointer to string being duplicated.
+ * Hint: man strdup
  *
- * Return: NULL if str is NULL.
- * pointer to duplicated string on success.
- * NULL if memory was insuffient.
+ * Return: NULL if str = NULL or memory was insufficient
+ *         pointer to duplicated string on success.
+ * Author: Gamachu AD
  */
 char *_strdup(char *str)
 {
@@ -19,20 +19,15 @@ char *_strdup(char *str)
 	/*check if str is NULL*/
 	if (str == NULL)
 		return (NULL);
-
 	len = 0;
 	while (str[len] != '\0')
 		len++;
-
 	nstr = malloc(sizeof(char) * (len + 1));
-
 	/*check if malloc was successful*/
 	if (nstr == NULL)
 		return (NULL);
-
 	for (i = 0; i < len; i++)
 		nstr[i] = str[i];
 	nstr[len] = '\0';
-
 	return (nstr);
 }
