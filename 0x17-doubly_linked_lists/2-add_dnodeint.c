4e4cffee06d8c382fe4b52a69063a8efe6fdabfb
Gamachu <jr.gamachu@gmail.com>
2024-01-22 20:49:10 +0530

    Add a new node at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,19 +1,19 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dll
  * @head: pointer to the head of dll
  * @n: the number to be inserted
  *
- * Return: the address of the new element, or NULL if it failed
- * Author: Jaba
+ * Return: the address of the new element
+ *         NULL if it failed
+ * Author: Gamachu AD
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	new = malloc(sizeof(dlistint_t));
+	new = malloc(sizeof(*new));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
@@ -22,6 +22,5 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (*head != NULL)
 		(*head)->prev = new;
 	*head = new;
-
 	return (new);
 }
