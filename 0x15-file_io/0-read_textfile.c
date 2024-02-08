f24cf01a9780624aa85f10458195c0675e5ff98b
Gamachu <jr.gamachu@gmail.com>
2024-02-08 19:55:04 +0530

    Reads a text

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,8 +7,7 @@
  *
  * Return: number of letters it could print or write on success.
  *         -1 on failure.
- * Author: Jaba
- * Date: Sep 12, 2022 or Fulbaana 02, 2015 ALH
+ * Author: Gamachu AD
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -17,27 +16,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
-
 	buf = malloc(letters * sizeof(char) + 1);
 	if (buf == NULL)
 		return (0);
-
 	rd = read(file, buf, letters);
 	if (rd == -1)
 		return (0);
-
 	buf[letters] = '\0';
-
 	wr = write(STDOUT_FILENO, buf, rd);
 	if (wr == -1)
 		return (0);
-
 	close(file);
 	free(buf);
-
 	return (wr);
 }
