9f64f47ee642f6752beb7700d1de3431e4d344c4
Gamachu <jr.gamachu@gmail.com>
2024-02-08 20:49:28 +0530

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,8 +6,7 @@
  * @text_content: string to be written to file
  *
  * Return: 1 on success, -1 on failure.
- * Author: Jaba
- * Date: Sep 02, 2022
+ * Author: Gamachu AD
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -15,11 +14,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
-
 	if (text_content)
 	{
 		i = 0;
@@ -29,7 +26,6 @@ int create_file(const char *filename, char *text_content)
 		if (wr != i)
 			return (-1);
 	}
-
 	close(file);
 	return (1);
 }
