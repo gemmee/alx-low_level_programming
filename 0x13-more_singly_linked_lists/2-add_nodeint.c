181e451244e5c67c61d11bc66968892ab2473a3a
Gamachu <jr.gamachu@gmail.com>
2024-02-18 01:21:52 +0530

    Add node at beginning

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * add_nodeint - adds a new node at the beginning of a list
- * @head: pointer to the head of the list
+ * @head: pointer to a pointer of the head of the list
  * @n: number to be inserted
  *
- * Return: the address of the new element, or NULL if it failed.
- * Author: Jaba
- * Date: Oct 05, 2022
+ * Return: the address of the new element,
+ *         or NULL if it failed.
+ * Author: Gamachu AD
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -17,11 +17,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
 	new->next = *head;
 	*head = new;
-
 	return (*head);
 }
 
