23a45d7d20039ecd258a2a4bf8cfb4194ed1e86a
Gamachu <jr.gamachu@gmail.com>
2024-02-20 19:58:22 +0530

    Insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,38 +16,33 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *temp, *node;
 	unsigned int count;
 
-	if (head == NULL)
-		return (NULL);
 	temp = *head;
 	count = 0;
-	while (temp)
-	{
-		count++;
-		temp = temp->next;
-	}
-	/* Handle a case of inserting at an index outside the list */
-	if (idx >= count)
-		return (NULL);
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
-		return (NULL);
-	node->n = n;
-	/* Handle a case of inserting at beginning of the list */
-	if (idx == 0)
-	{
-		node->next = *head;
-		*head = node;
-		return (node);
-	}
-	count = 0;
-	temp = *head;
-	/* Move temp just before the position of insertion */
+	/* Move temp just before point of insertion */
 	while (temp && count < idx - 1)
 	{
-		temp = temp->next;
 		count++;
+		temp = temp->next;
 	}
-	node->next = temp->next;
-	temp->next = node;
-	return (node);
+	/* Allocate memory for the node to inserted */
+	node = malloc(sizeof(listint_t));
+	if (node)
+	{
+		node->n = n;
+		/* Handle a case of inserting at beginning of the list */
+		if (idx == 0)
+		{
+			node->next = *head;
+			*head = node;
+			return (node);
+		}
+		if (idx == count + 1)
+		{
+			node->next = temp->next;
+			temp->next = node;
+			return (node);
+		}
+	}
+	free(node);
+	return (NULL);
 }
