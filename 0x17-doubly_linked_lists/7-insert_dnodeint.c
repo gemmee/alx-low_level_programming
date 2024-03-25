33acb16716b5e83b9b288efdec4cb2b2c6ddbb7e
Gamachu <jr.gamachu@gmail.com>
2024-03-25 18:45:41 +0530

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,62 +1,53 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: head of the list
- * @idx: index of the new node
- * @n: value of the new node
+ * insert_dnodeint_at_index - inserts a new node at a given index
+ * @idx: the index of insertion
+ * @n: value to be inserted
+ * @h: a pointer to a pointer to the head of the list
  *
- * Return: the address of the new node, or NULL if it failed
- * Author: Jaba {I did not write the code myself, even understanding
- * some part of it was a difficult for me at this time}.
- * Credit: @mecomonteshbtn on github
- * Description: I am commenting out (deleted) the first code because
- * it's difficult to
- * understand and also it failed one of the checks.
- * Credit to @Abinet508 for the the second one, which is more elegant.
+ * Return: the address of the new node
+ *         NULL if it failed
+ * Author: Gamachu AD
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
-	dlistint_t *head;
-	unsigned int i;
+	dlistint_t *new, *pre;
+	unsigned int count;
 
-	new = NULL;
+	if (h == NULL || *h == NULL)
+		return (NULL);
+
+	/*insertion at beginning*/
 	if (idx == 0)
-		new = add_dnodeint(h, n);
-	else
+		return (add_dnodeint(h, n));
+
+	/*insertion at end*/
+	count = 0;
+	pre = *h;
+	while (pre->next)
 	{
-		head = *h;
-		i = 1;
-		if (head != NULL)
-			while (head->prev != NULL)
-				head = head->prev;
-		while (head != NULL)
-		{
-			if (i == idx)
-			{
-				if (head->next == NULL)
-					new = add_dnodeint_end(h, n);
-				else
-				{
-					new = malloc(sizeof(dlistint_t));
-					if (new != NULL)
-					{
-						new->n = n;
-						new->next = head->next;
-						new->prev = head;
-						head->next->prev = new;
-						head->next = new;
-					}
-				}
-				break;
-			}
-			head = head->next;
-			i++;
-		}
+		pre = pre->next;
+		count++;
 	}
+	if (count <= idx)
+		return (NULL);
+	if (idx == count)
+		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(*new));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	pre = *h;
+	count = 0;
+	while (++count != idx)
+		pre = pre->next;
+	new->next = pre->next;
+	pre->next->prev = new;
+	new->prev = pre;
+	pre->next = new;
 
 	return (new);
 }
