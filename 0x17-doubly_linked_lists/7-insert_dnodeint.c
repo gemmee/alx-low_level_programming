b2f20505148d81f42203ee387d45194a833c6a21
Gamachu <jr.gamachu@gmail.com>
2024-03-25 19:07:26 +0530

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new, *pre;
 	unsigned int count;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 		return (NULL);
 
 	/*insertion at beginning*/
