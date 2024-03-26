82832960a785de0d401bb7ad7624550cb4cbf59c
Gamachu <jr.gamachu@gmail.com>
2024-03-26 14:14:20 +0530

    Find largest palindrome

---
+++ 0x17-doubly_linked_lists/102-palindrome.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# Finds the largest palindrome made from the
+# product of two 3-digit numbers
+
+def is_palindrome(n):
+	n = str(n)
+	return n == n[::-1]
+
+n = 100
+while (n <= 999):
+	m = 100
+	while (m <= 999):
+		if is_palindrome(n*m):
+			max = n*m
+			n_max = n
+			m_max = m
+		m += 1
+	n += 1
+
+# the loop checks for 900 * 900 times
+print("max = ", max)
+print("n_max = ", n_max)
+print("m_max = ", m_max)		
