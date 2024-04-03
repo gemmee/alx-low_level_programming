76ea572aed04b9edaeb5e9995710efcc100fb87d
Gamachu <jr.gamachu@gmail.com>
2024-04-03 23:36:36 +0530

    Pass a function pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,14 @@
-/**
-  *print_name - prints a name.
-  *@name: pointer to name.
-  *@f: function pointer.
-  *
-  *Return: void
-  *Author: Jaba
-  *Date: Aug 29 2022 @kp10b
-  */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-		f(name);
-}
+#include "function_pointers.h"
 
+/**
+ * print_name - a function that prints a name
+ * @name: a pointer to a character
+ * @f: a pointer to a function
+ *
+ * Return: Nothing
+ * Author: Gamachu AD
+ */
+void print_name(char *name, void (*f)(char *s))
+{
+	f(name); /* equivalent to (*f)(name); */
+}
