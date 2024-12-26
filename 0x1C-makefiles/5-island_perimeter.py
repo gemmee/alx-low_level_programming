0ccd7739ef599258238391669cd4a9f33d0f6dc8
Gamachu <jr.gamachu@gmail.com>
2024-12-26 22:36:13 +0300

    Fix pycodestyle issues

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,38 +3,41 @@
 Module for calculating the perimeter of an island in a grid.
 """
 
+
 def island_perimeter(grid):
-	"""
-	Calculate the perimeter of the island described in grid.
+    """
+    Calculate the perimeter of the island described in grid.
 
-	Args:
-		grid (list of list of int): A 2D grid where 0 represents water and 1 represents land.
+    Args:
+        grid (list of list of int): A 2D grid where 0 represents\
+        water and 1 represents land.
 
-	Returns:
-		int: The perimeter of the island.
-	"""
-	rows = len(grid)
-	cols = len(grid[0])
-	perimeter = 0
+    Returns:
+        int: The perimeter of the island.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
 
-	for r in range(rows):
-		for c in range(cols):
-			if grid[r][c] == 1:
-				# check all four directions
-				# Top
-				if r == 0 or grid[r - 1][c] == 0:
-					perimeter += 1
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                # check all four directions
 
-				# Bottom
-				if r == rows - 1 or grid[r + 1][c] == 0:
-					perimeter += 1
+                # Top
+                if r == 0 or grid[r - 1][c] == 0:
+                    perimeter += 1
 
-				# Left
-				if c == 0 or gric[r][c - 1] == 0:
-					perimeter += 1
+                # Bottom
+                if r == rows - 1 or grid[r + 1][c] == 0:
+                    perimeter += 1
 
-				# Right
-				if c == cols - 1 or grid[r][c + 1] == 0:
-					perimeter += 1
+                # Left
+                if c == 0 or gric[r][c - 1] == 0:
+                    perimeter += 1
 
-	return perimeter
+                # Right
+                if c == cols - 1 or grid[r][c + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
